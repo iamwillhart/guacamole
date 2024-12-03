8f310c54d13a91413914f836779abc82f377d844
Will Thomas <willthomas@supermarket.london>
2024-12-03 21:43:52 +0000

    Added Google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import GoogleAnalytics from './components/GoogleAnalytics'
 
 export const metadata = {
   title: 'Hedgehog Fundamentals',
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-Z7GNF0YK8Y" />
         {children}
       </body>
     </html>
